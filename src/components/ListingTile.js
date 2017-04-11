ff2e084f6ffa9417421b7408951a62f783135842
Zach Gawlik <zachgawlik@gmail.com>
2017-04-11 11:24:34 -0400

    Moving ListingTile content to top

--- src/components/ListingTile.js
+++ src/components/ListingTile.js
@@ -11,16 +11,18 @@ export default class ListingTile extends React.Component {
     const { listing } = this.props;
     return (
       <div className="ListingTile">
+        <div className="ListingTile__content">
+          <div className="ListingTile__headline">
+            <p className="ListingTile__title overflow-ellipsis">
+              {listing.title}
+            </p>
+            <ColorIcon isColor={listing.isColor} />
+          </div>
+        </div>
         <ListingThumbnail
           title={listing.title}
           thumbnailId={listing.thumbnailId}
         />
-        <div className="ListingTile__content">
-          <p className="ListingTile__title">
-            {listing.title}
-          </p>
-          <ColorIcon isColor={listing.isColor} />
-        </div>
       </div>
     );
   }
